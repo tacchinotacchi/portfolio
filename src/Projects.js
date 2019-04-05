513cd0c90d7dd71c26c70f253f30911a7f2bcc97
Alex AMADORI <aamadori@student.42.fr>
2019-04-05 21:53:25 +0200

    Write some  stuff

--- src/Projects.js
+++ src/Projects.js
@@ -37,18 +37,18 @@ class Projects extends Component {
 	renderProjects () {
 		let test = [
 			new ProjectDescription(
-				null, "corewar description placeholder"
-			),
-			new ProjectDescription(
 				require("./resources/lemin.mp4"),
-				"Lem-in is a solver for a routing problem, where a number of resources (\"ants\") have to be routed from a start node to an end node, in an undirected graph where the nodes have capacity of 1 resource, with the smallest amount of instructions (\"turns\") possible."
+				"Lem-in is a solver for a routing problem, where a number of resources (\"ants\") have to be routed from a start node to an end node, in an undirected graph where the nodes have capacity of 1 resource, with the smallest amount of instructions (\"turns\") possible. The lowest cost solution is found by first computing the min-cost max-flow of the graph, and then scheduling the movement of the resources with a greedy algorithm to parallelize the instructions as much as possible. In order to facilitate debugging and visualization, the 3d visualizer uses linear regression to try and find a configuration with optimal distances for connected and non connected nodes, trying to make paths more easily visible."
 			),
 			new ProjectDescription(
-				require("./resources/filler.mp4"), "genetic algo filler champs"
+				require("./resources/filler.mp4"), "In the game of Filler, two \"champions\" use random tetris-like pieces supplied randomly to try and conquer as much of the field as possible. While a basic min-max algorithm can go a long way, due to the randomness of the game it's impossible to look more than one move ahead. In order to get a statistical advantage, fuzzy parameters can be introduced to the algorithm and then evolved with a genetic algorithm. Simulating games takes more computing power than it looks, however due to the number of parameters a handful of generations are enough to get good results."
 			),
 			new ProjectDescription(
 				null, "42 holy tree"
-			)
+			),
+			new ProjectDescription(
+				null, "Corewars coming soon!"
+			),
 		]
 		let result = test.map((elem, index) => {
 			return (
