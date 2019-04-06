e1706dff8c31f5856c87915ec9329d79f0405b7c
Alex AMADORI <aamadori@student.42.fr>
2019-04-06 18:32:09 +0200

    Updates

---
+++ precache-manifest.cdf1a5538744a875a0f90da74c2d2ec3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb92313aea4876eb25ab",
+    "url": "/portfolio/static/css/main.d4a91fa0.chunk.css"
+  },
+  {
+    "revision": "bb92313aea4876eb25ab",
+    "url": "/portfolio/static/js/main.83262869.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "6a26a5050b6f20324946",
+    "url": "/portfolio/static/js/2.6020ccb5.chunk.js"
+  },
+  {
+    "revision": "e384ad67a7d896e436e50b27b5a7775d",
+    "url": "/portfolio/static/media/lemin.e384ad67.mp4"
+  },
+  {
+    "revision": "5a97e3bf8ef5d801c1fb5e14808411cd",
+    "url": "/portfolio/static/media/filler.5a97e3bf.mp4"
+  },
+  {
+    "revision": "65215e745ee37df0e3948855a98fb4e3",
+    "url": "/portfolio/static/media/42_Logo.65215e74.svg"
+  },
+  {
+    "revision": "1de6e30bd91f13d8609a7dbece6227d6",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.02582a7ef92c7cddbf015876f2726f26.js"
+  "/portfolio/precache-manifest.cdf1a5538744a875a0f90da74c2d2ec3.js"
 );
 
 workbox.clientsClaim();
